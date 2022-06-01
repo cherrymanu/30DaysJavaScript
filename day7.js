a7312cc1dc6e4545b8c40f0d46302ef710753296
cherrymanu <charishmamanupati11@gmail.com>
2022-06-01 13:56:09 +0530

    day7

--- day7.js
+++ day7.js
@@ -1,1 +1,54 @@
-console.log('day7 code')
+console.log('day7 code');
+function printFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`
+  }
+  console.log(printFullName('Asabeneh', 'Yetayeh'));
+  //level1
+
+  function fullName(){
+      console.log("printing full name")
+  }
+  fullName();
+  function addNumbers(num1,num2){
+      return num1+num2
+  }
+  console.log(addNumbers(3,2));
+  function areaOfCircle(radius){
+      return(Math.PI*radius*radius);
+  }
+  console.log(areaOfCircle(7));
+
+
+  function bmi(weight,height){
+      let bmi=weight/(height*height);
+      console.log(bmi)
+      if(bmi<18.5){
+          console.log('under weight')
+      }
+      else if(18.5<bmi<24.9){
+          console.log('Normal weight')
+      }
+      else if(25<bmi<29.9){
+          console.log('overweight')
+      }
+      else{
+          console.log('obese')
+      }
+  }
+  bmi(53,15.3);
+
+  let array1 = [2,3,4,5,6];
+  function printArray(array){
+      console.log(array)
+  }
+  printArray(array1)
+
+  function sum(){
+      let sum1=0;
+      for(let i=0;i<arguments.length;i++){
+          sum1+=arguments[i]
+      }
+      console.log(sum1)
+  }
+  sum(2,4,5)
+
